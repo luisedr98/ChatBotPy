841f319c65be339ed3b894c3a611c8891070b376
luisedr98 <luisdominguez818@gmail.com>
2022-11-20 16:05:03 -0500

    add: configuracion para entrenar al bot

--- README.md
+++ README.md
@@ -7,10 +7,23 @@ El presente proyecto presenta el desarrollo de un chatBot mediante el uso del fr
 - Desarrollado en lenguaje python v3.7.9
 - Es necesario para su funcionamiento la creación de un entorno virtual y descarga de dedpendecias.
 
-## Descarga del entorno virtual y paquetes necesarios
+## Preparacion del entorno virtual y paquetes necesarios
 
-Para el uso es necesario seguir el siguiente procedimiento. Ingresar a la carpeta donde se encuentra la carpeta "hospital" y mediante el el uso del comand prompt construir un entorno virtual mediante la ejecución de los siguientes comandos
+Para el uso es necesario seguir el siguiente procedimiento. Ingresar a la carpeta donde se encuentra la carpeta "hospital" y mediante el el uso del comand prompt construir un entorno virtual mediante la ejecución de los siguientes comandos:
 
     python -m venv nombre_del_entorno
     .\nombre_del_entorno\Scripts\activate
     python -m pip install rasa
+
+## Ejecución del ChatBot
+
+Para la ejecución del ChatBot es necesario ejecutar los siguientes comandos en la carpeta donde se encuentra el entorno virtual y el proyecto
+
+    .\nombre_del_entorno\Scripts\activate
+    cd hospital
+    rasa train
+    
+Luego de la ejecución del comando ***rasa train*** se generará las carpetas **.rasa** y **models** en la ultima se encuentra el resultado del entrenamiento. Una vez generado directorios es necesario ejecutar
+
+    rasa run actions 
+    rasa run shell
--- hospital/actions/actions.py
+++ hospital/actions/actions.py
@@ -9,20 +9,57 @@
 
 from typing import Any, Text, Dict, List
 
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+dataFacilities = [
+    {
+        'location': "Ambato",
+        'facility': "hospital",
+        'address': "Av de Prueba y Otra",
+    },
+    {
+        'location': "Ambato",
+        'facility': "clinical",
+        'address': "Av de Prueba y Av. Eloy Alfaro",
+    },
+        {
+        'location': "Ambato",
+        'facility': "laboratory",
+        'address': "Av de Prueba y Av. Cevallos",
+    },
+    {
+        'location': "Cuenca",
+        'facility': "clinical",
+        'address': "Av de Guaynacapac y Av. Eloy Alfaro",
+    },
+        {
+        'location': "Cuenca",
+        'facility': "laboratory",
+        'address': "Av De los Pinos y Av. Cevallos",
+    },
+]
+
+def get_address(location, facility):
+    for facilityResponse in dataFacilities:
+        if facilityResponse.get("facility") == facility and facilityResponse.get("location") == location:
+            return facilityResponse.get("address")
+    return ''
 
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_facility"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        facility = tracker.get_slot("facility")
+        location = tracker.get_slot("location")
 
-        dispatcher.utter_message(text="Hello World!")
+        resp = get_address(location, facility)
+
+        dispatcher.utter_message(text=resp)
 
         return []
